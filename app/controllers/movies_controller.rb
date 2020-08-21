64414aa3118eb879cba7b6a383943991736e1507
AJ Tran <ajtrandev@gmail.com>
2020-08-20 18:19:12 -0600

    Add feature: Clicking the 'Find Top Rated Movies' shows top rated results
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,19 @@
 class MoviesController < ApplicationController
-  def index; end
+  def index
+    query_params = params[:q]
+
+    url = "https://api.themoviedb.org"
+
+    conn = Faraday.new(url) do |f|
+      f.params["api_key"] = ENV["TMDB_API_KEY"]
+    end
+
+    response_1 = conn.get "/3/movie/top_rated?page=1"
+    response_2 = conn.get "/3/movie/top_rated?page=2"
+
+    movies_1 = JSON.parse(response_1.body, symbolize_names: true)
+    movies_2 = JSON.parse(response_2.body, symbolize_names: true)
+
+    @movies = movies_1[:results] + movies_2[:results]
+  end
 end
