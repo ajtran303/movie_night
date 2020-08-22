84eb24c9f237f3fc5ba0045a9eeb5bd12c765ad2
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-22 12:42:11 -0600

    Refactor TheMoviesDataBaseService into services
    Co-authored-by: AJ Tran <31839316+ajtran303@users.noreply.github.com>

---
+++ app/services/the_movies_data_base_service.rb
@@ -0,0 +1,47 @@
+class TheMoviesDataBaseService
+  def top_rated_movies
+    movies = [
+      conn.get('/3/movie/top_rated?page=1'),
+      conn.get('/3/movie/top_rated?page=2')
+    ]
+    movies.sum(&movies_results)
+  rescue NoMethodError
+    nil
+  end
+
+  def find_movies(keyword)
+    movies = [
+      conn.get("/3/search/movie?page=1&query=#{keyword}"),
+      conn.get("/3/search/movie?page=2&query=#{keyword}")
+    ]
+    movies.sum(&movies_results)
+  rescue NoMethodError
+    nil
+  end
+
+  private
+
+  def conn
+    Faraday.new 'https://api.themoviedb.org' do |f|
+      f.params['api_key'] = ENV['TMDB_API_KEY']
+      f.headers['Content-Type'] = 'application/json'
+    end
+  end
+
+  def parse_body(response)
+    JSON.parse(response.body, symbolize_names: true) if valid_json?(response.body)
+  end
+
+  def movies_results
+    proc do |movies_response|
+      movies = parse_body(movies_response)
+      movies[:results] unless movies.nil?
+    end
+  end
+
+  def valid_json?(string)
+    JSON.parse(string)
+  rescue JSON::ParserError
+    false
+  end
+end
