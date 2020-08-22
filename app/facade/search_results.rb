8074d3ecdb78e95d1b36d8b378d2727050bac88d
AJ Tran <ajtrandev@gmail.com>
2020-08-22 13:50:30 -0600

    Add guard clauses to SearchResults facade

--- app/facade/search_results.rb
+++ app/facade/search_results.rb
@@ -6,13 +6,19 @@ class SearchResults
   end
 
   def self.find_movies_by_title(keyword)
-    service_movie_data.find_movies_by_title(keyword).map do |movie_data|
+    movie_title_search_results = service_movie_data.find_movies_by_title(keyword)
+    return nil if movie_title_search_results.nil?
+
+    movie_title_search_results.map do |movie_data|
       Movie.new(movie_data)
     end
   end
 
   def self.top_rated_movies
-    service_movie_data.top_rated_movies.map do |movie_data|
+    top_movies = service_movie_data.top_rated_movies
+    return nil if top_movies.nil?
+
+    top_movies.map do |movie_data|
       Movie.new(movie_data)
     end
   end
