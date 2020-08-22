b20ab894296646e2c03f8abc0dfeefb5932db1de
AJ Tran <ajtrandev@gmail.com>
2020-08-22 15:50:13 -0600

    Remove redundant guard clauses from SearchResults
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>

--- app/facade/search_results.rb
+++ app/facade/search_results.rb
@@ -7,8 +7,6 @@ class SearchResults
 
   def self.find_movies_by_title(keyword)
     movie_title_search_results = service_movie_data.find_movies_by_title(keyword)
-    return nil if movie_title_search_results.nil?
-
     movie_title_search_results.map do |movie_data|
       Movie.new(movie_data)
     end
@@ -16,8 +14,6 @@ class SearchResults
 
   def self.top_rated_movies
     top_movies = service_movie_data.top_rated_movies
-    return nil if top_movies.nil?
-
     top_movies.map do |movie_data|
       Movie.new(movie_data)
     end
