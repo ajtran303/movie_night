3ee01488f001e4e869185da205092caf819e0ea8
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-23 12:05:07 -0600

    Create friendship model Model contains validations for user_id and friend_id Friend_id is a user_id from the user table Relationships built for user and friend two methods added to create a friendship and to destroy a friendship

---
+++ app/models/friendship.rb
@@ -0,0 +1,20 @@
+class Friendship < ApplicationRecord
+  validates :user_id, uniqueness: true, presence: true
+  validates :friend_id, uniqueness: true, presence: true
+
+  belongs_to :user
+  belongs_to :friend, class_name: 'User', foreign_key: :friend_id
+
+  def self.create_reciprocal_for_ids(user_id, friend_id)
+    user_friendship = Friendship.create(user_id: user_id, friend_id: friend_id)
+    friend_friendship = Friendship.create(user_id: friend_id, friend_id: user_id)
+    [user_friendship, friend_friendship]
+  end
+
+  def self.destroy_reciprocal_for_ids(user_id, friend_id)
+    friendship1 = Friendship.find_by(user_id: user_id, friend_id: friend_id)
+    friendship2 = Friendship.find_by(user_id: friend_id, friend_id: user_id)
+    friendship1.destroy
+    friendship2.destroy
+  end
+end
