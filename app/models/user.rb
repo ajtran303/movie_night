f37cd9e61501194fea066d781f7a653e7dc32839
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-23 12:00:58 -0600

    Update user model with email and friends/friendship relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :user_id, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :name, presence: true
+
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 end
