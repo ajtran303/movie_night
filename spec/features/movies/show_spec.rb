089008ff6b825d0104d67d79bae7decc71d64e32
AJ Tran <ajtrandev@gmail.com>
2020-08-24 16:27:49 -0600

    Use VCR cassettes for features/movies/show_spec.rb
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -2,16 +2,14 @@ require "rails_helper"
 
 RSpec.describe "Movie Show Page" do
   describe "As an authenticated user" do
-    before :each do
+    it "I can see movie details", :vcr do
       @user = User.create!(user_id: "100000000000000000000", name: "John Smith")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       visit movies_path
       click_on "Top Rated"
       click_on "Gabriel's Inferno Part II"
       expect(current_path).to eq('/movies/724089')
-    end
 
-    it "I can see movie details" do
       expect(page).to have_content("")
     end
   end
