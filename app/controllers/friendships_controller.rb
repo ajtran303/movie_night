c361e6951e722902c4bd15228fb693627ea3025c
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-24 20:04:10 -0600

    Refactor create action for friendships controller
    Move nested if statement into private method
    
    Co-authored-by: AJ Tran <31839316+ajtran303@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,15 +2,21 @@ class FriendshipsController < ApplicationController
   def create
     if User.exists?(email: params[:friends_email])
       friend = User.find_by(email: params[:friends_email])
-      if friend.id == current_user.id
-        flash[:error] = "You cannot add yourself as a friend"
-      else
-        Friendship.create_reciprocal_for_ids(current_user.id, friend.id)
-        flash[:success] = "You are now friends with #{friend.name}"
-      end
+      create_friendship(current_user.id, friend)
     else
       flash[:failure] = 'Email does not exist, please try another one'
     end
     redirect_to dashboard_index_path
+  end
+
+  private
+
+  def create_friendship(user_id, friend)
+    if friend.id == user_id
+      flash[:error] = 'You cannot add yourself as a friend'
+    else
+      Friendship.create_reciprocal_for_ids(user_id, friend.id)
+      flash[:success] = "You are now friends with #{friend.name}"
+    end
   end
 end
