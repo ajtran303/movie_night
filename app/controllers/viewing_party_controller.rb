ea34a28fe0eb014008be5af6c2cbe3f084d68d3a
AJ Tran <ajtrandev@gmail.com>
2020-08-26 17:33:49 -0600

    Add create action to ViewingParty controller
      It's only the happy path
      TODO: add tests for sad path
    
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -4,46 +4,14 @@ class ViewingPartyController < BaseController
   end
 
   def create
-    # date = params[:date] "2020-08-27"
-    # time = params[:time] "17:06"
-    # params[:movie_title] = ""
-    # params[:duration_of_party] = ""
-  #   new_party = Party.create({
-  #     title: params[:title],
-  #     party_date: params[:party_date],
-  #     party_time: params[:party_time],
-  #     attendees: params[:friend_id].values,
-  #     user_id: current_user.id
-  #     }
-  #   )
-  #   if new_party.save
-  #     flash[:success] = "Your party for #{params[:title]} on #{params[:party_date]} at #{params[:party_time]} was created"
-  #     redirect_to dashboard_path
-  #     # post_to_calendar
-  #     CalendarService.create_viewing_party(new_party)
-  #   end
+    party = Party.new(viewing_party_params)
+    party.save if party.valid?
   end
-end
 
-class CalendarService
-  def self.create_viewing_party(event)
-    calendar = Google::Apis::CalendarV3
-    client = calendar::CalendarService.new
-    client.authorization = access_token
+  private
 
-    viewing_party = Google::Apis::CalendarV3::Event.new(
-      summary: 'Viewing Party With Friends',
-      location: 'Internet',
-      description: event.title,
-      start: Google::Apis::CalendarV3::EventDateTime.new(
-        date_time: event.start_time,
-        time_zone: 'America/Denver'
-      ),
-      end: Google::Apis::CalendarV3::EventDateTime.new(
-        date_time: event.end_time,
-        time_zone: 'America/Denver'
-      )
-    )
-    client.insert_event('primary', viewing_party)
+  def viewing_party_params
+    p = params.permit(:movie_title, :party_duration, :date, :time)
+    p.merge({user_id: current_user.id})
   end
 end
