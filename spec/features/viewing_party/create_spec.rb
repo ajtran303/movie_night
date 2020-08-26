e1f8396e040455e50518973d13aee120e199889a
AJ Tran <ajtrandev@gmail.com>
2020-08-26 16:36:31 -0600

    Add setup for Viewing Party Create Spec
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>

--- spec/features/viewing_party/create_spec.rb
+++ spec/features/viewing_party/create_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Create Viewing Party Spec" do
+RSpec.describe "Create Viewing Party Spec", :vcr, :new_episodes => true do
   describe "As an authenticated user" do
     before :each do
       @user1 = User.create!(oauth_id: "100000000000000000000", name: "John Smith", email: "john@example.com", access_token: "TOKEN", refresh_token: "REFRESH_TOKEN")
@@ -8,6 +8,29 @@ RSpec.describe "Create Viewing Party Spec" do
       Friendship.create_reciprocal_for_ids(@user1.id, @user2.id)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
+      visit discover_index_path
+      within ".find-movies" do
+        fill_in :movie_title_keywords, with: "Buffy"
+        click_on "Find Movies"
+      end
+
+      VCR.use_cassette('Buffy Show Page') do
+        within ".results" do
+          movie = page.first(:xpath, '//article')
+          movie_link = movie.find_link.text
+          click_on movie_link
+        end
+      end
+
+      VCR.use_cassette('New Viewing Party Page') do
+        within ".movie-details" do
+          click_on "Create A Viewing Party"
+        end
+      end
     end
+
+    # scenario "There is a form to create a viewing party" do
+    # end
   end
 end
