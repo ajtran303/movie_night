e8f4e67005d99ea895e4f608ac5f9200f15555c4
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-26 19:13:55 -0600

    Add route to sessions destroy method
    Route created for logout feature
    
    Co-authored-by: AJ Tran <31839316+ajtran303@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get "/auth/google_oauth2/callback", to: "sessions#create"
-
+  delete '/sessions', to: 'sessions#destroy'
   resources :dashboard, only: :index
   resources :friendships, only: :create
   resources :discover, only: :index
