f18471e80db402b3cb2e7f96202479fef4f4e3e9
AJ Tran <ajtrandev@gmail.com>
2020-08-26 21:41:05 -0600

    Add spec to add to calendar button conditional logic
    Co-authored-by: Kathy Bui <61847658+Kathybui732@users.noreply.github.com>

--- spec/features/dashboard/viewing_party_section_spec.rb
+++ spec/features/dashboard/viewing_party_section_spec.rb
@@ -2,27 +2,33 @@ require "rails_helper"
 
 RSpec.describe "Dashboard Page Viewing Party Section Spec" do
   before :each do
-    @user1 = User.create!(oauth_id: "100000000000000000000", name: "John Smith", email: "john@example.com", access_token: "TOKEN", refresh_token: "REFRESH_TOKEN")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
     current_date = DateTime.now.to_date.to_s
     current_time = DateTime.now.to_time.to_s[11..15]
+    @user1 = User.create!(oauth_id: "100000000000000000000", name: "John Smith", email: "john@example.com", access_token: "TOKEN", refresh_token: "REFRESH_TOKEN")
+    @user2 = User.create!(oauth_id: "100000000000000000002", name: "Jane Bulldozer", email: "jane@example.com", access_token: "TOKEN", refresh_token: "REFRESH_TOKEN")
     @party1 = Party.create!(user: @user1, date: current_date, time: current_time, movie_title: "Resident Evil", party_duration: 180 )
-    @party2 = Party.create!(user: @user1, date: current_date, time: current_time, movie_title: "Resident Evil 2", party_duration: 180 )
-    @party3 = Party.create!(user: @user1, date: current_date, time: current_time, movie_title: "Resident Evil 3", party_duration: 180 )
-    @parties = [@party1, @party2, @party3]
+    PartyInvitee.create(party_id: @party1.id, invitee_id: @user2.id)
+    @party2 = Party.create!(user: @user2, date: current_date, time: current_time, movie_title: "John Wick 3", party_duration: 200 )
+    PartyInvitee.create(party_id: @party2.id, invitee_id: @user1.id)
   end
 
-  describe "As a logged in user on the dashboard page" do
-    it "There is a section with all my viewing parties" do
-      visit dashboard_index_path
+  scenario "Add to calendar button exists properly" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+    visit dashboard_index_path
 
-      within ".viewing-party" do
-        @parties.each do |party|
-          expect(page).to have_content(party.movie_title)
-          expect(page).to have_content(party.date)
-          expect(page).to have_content(party.time)
-          expect(page).to have_button("Add to Calendar")
-        end
+    within ".viewing-party" do
+      within "#party-#{@party1.id}" do
+        expect(page).to have_content(@party1.movie_title)
+        expect(page).to have_content(@party1.date)
+        expect(page).to have_content(@party1.time)
+        expect(page).to_not have_button("Add to Calendar")
+      end
+
+      within "#party-#{@party2.id}" do
+        expect(page).to have_content(@party2.movie_title)
+        expect(page).to have_content(@party2.date)
+        expect(page).to have_content(@party2.time)
+        expect(page).to have_button("Add to Calendar")
       end
     end
   end
